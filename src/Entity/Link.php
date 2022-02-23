791ab5cbd3d611270cc70c3d9cd334ce04166106
Gallien L <1328920+glb2154@users.noreply.github.com>
2022-02-23 15:38:15 +0100

    gsoi: remove idea folder + add wip

--- src/Entity/Link.php
+++ src/Entity/Link.php
@@ -6,12 +6,152 @@ namespace App\Entity;
 use App\Repository\LinkRepository;
 use Doctrine\ORM\Mapping as ORM;
 
+/**
+ * @ORM\Entity(repositoryClass=LinkRepository::class)
+ * @ORM\Table(name="link")
+ * @ORM\HasLifecycleCallbacks
+ */
 class Link
 {
-    private string $url;
-    private string $providerName;
-    private string $title;
-    private string $author;
-    private $createdAt;
-    private $publishedAt;
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", name="provider", length=2048, nullable=true)
+     */
+    private ?string $url;
+
+    /**
+     * @return string|null
+     */
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    /**
+     * @param string|null $url
+     */
+    public function setUrl(?string $url): void
+    {
+        $this->url = $url;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getProvider(): ?string
+    {
+        return $this->provider;
+    }
+
+    /**
+     * @param string|null $provider
+     */
+    public function setProvider(?string $provider): void
+    {
+        $this->provider = $provider;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string|null $title
+     */
+    public function setTitle(?string $title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param string|null $author
+     */
+    public function setAuthor(?string $author): void
+    {
+        $this->author = $author;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param \DateTime $createdAt
+     */
+    public function setCreatedAt(\DateTime $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getPublishedAt(): \DateTime
+    {
+        return $this->publishedAt;
+    }
+
+    /**
+     * @param \DateTime $publishedAt
+     */
+    public function setPublishedAt(\DateTime $publishedAt): void
+    {
+        $this->publishedAt = $publishedAt;
+    }
+
+    /**
+     * @ORM\Column(type="string", name="provider", length=255, nullable=true)
+     */
+    private ?string $provider;
+
+    /**
+     * @ORM\Column(type="string", name="title", length=255, nullable=true)
+     */
+    private ?string $title;
+
+    /**
+     * @ORM\Column(type="string", name="author", length=255, nullable=true)
+     */
+    private ?string $author;
+
+    /* @ORM\Column(type="datetime", name="created_at") */
+    private \DateTime $createdAt;
+
+    /* @ORM\Column(type="datetime", name="published_at") */
+    private \DateTime $publishedAt;
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function onPrePersist()
+    {
+        $this->createdAt = new \DateTime("now");
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
 }
